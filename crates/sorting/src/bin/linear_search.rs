dbac88fac951258f84005b683ec92c7a12d3d37d
Reed O'Brien <reed@reedobrien.com>
2023-11-20 09:21:49 -0800

    chore: stub out linear search

---
+++ crates/sorting/src/bin/linear_search.rs
@@ -0,0 +1,6 @@
+use anyhow::Result;
+// https://liveproject.manning.com/module/1552_5_1/sorting-and-searching-rust/4--linear-search/4-1-workflow%3a-linear-search
+
+fn main() -> Result<()> {
+    Ok(())
+}
