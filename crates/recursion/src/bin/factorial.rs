f52eaa1527c85425f9f8fc2b0c4950d052296a7b
Reed O'Brien <reed@reedobrien.com>
2023-11-21 20:41:22 -0800

    feat: Add factorial
    Used u128 to avoid panic and dealing with negative numbers.

--- crates/recursion/src/bin/factorial.rs
+++ crates/recursion/src/bin/factorial.rs
@@ -1,5 +1,13 @@
-use anyhow::Result;
+fn main() {
+    for n in 0..22 {
+        println!("{n}! = {}", factorial(n));
+    }
+}
 
-fn main() -> Result<()> {
-    Ok(())
+fn factorial(n: u128) -> u128 {
+    if n == 0 {
+        return 1;
+    }
+
+    n * factorial(n - 1)
 }
