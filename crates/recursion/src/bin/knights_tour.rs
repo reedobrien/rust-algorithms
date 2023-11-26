c363043a561505581d756e524c6af145a51715bf
Reed O'Brien <reed@reedobrien.com>
2023-11-25 23:17:58 -0800

    feat: Add initial knight's tour impl
    Seems to work on 8x8 hardwired board.
    Panics on 3-7.

---
+++ crates/recursion/src/bin/knights_tour.rs
@@ -0,0 +1,251 @@
+use std::{fmt, time::Instant};
+
+use anyhow::Result;
+
+const MOVES: [(isize, isize); 8] = [
+    (-1, -2),
+    (-1, 2),
+    (-2, -1),
+    (-2, 1),
+    (1, -2),
+    (1, 2),
+    (2, -1),
+    (2, 1),
+];
+
+const SIZE: usize = 8;
+const UNVISITED: isize = -1;
+
+struct Board {
+    cells: [[isize; SIZE]; SIZE],
+}
+
+impl Board {
+    fn new() -> Self {
+        Self {
+            cells: [[UNVISITED; SIZE]; SIZE],
+        }
+    }
+
+    fn moves(&self, pos: Cell) -> Vec<Cell> {
+        MOVES
+            .iter()
+            .filter(|m| self.valid_move(&pos, m))
+            .map(|m| Cell {
+                x: pos.x + m.0,
+                y: pos.y + m.1,
+            })
+            .collect()
+    }
+
+    // Get the next move using Warnsdorf's rule.
+    fn next_move(&self, pos: Cell) -> Option<Cell> {
+        struct Candidate {
+            cell: Cell,
+            paths: usize,
+        }
+        let mut candidates: Vec<Candidate> = vec![];
+
+        let moves = self.moves(pos);
+        if moves.len() < 1 {
+            return None;
+        }
+
+        for m in moves {
+            candidates.push(Candidate {
+                cell: m,
+                paths: self.moves(m).len(),
+            });
+        }
+
+        candidates.sort_unstable_by_key(|c| c.paths);
+
+        Some(candidates[0].cell)
+    }
+
+    fn valid_move(&self, pos: &Cell, r#move: &(isize, isize)) -> bool {
+        if pos.x + r#move.0 > 7 || pos.x + r#move.0 < 0 {
+            return false;
+        }
+        if pos.y + r#move.1 > 7 || pos.y + r#move.1 < 0 {
+            return false;
+        }
+
+        if self.visited(&Cell {
+            x: pos.x + r#move.0,
+            y: pos.y + r#move.1,
+        }) {
+            return false;
+        }
+
+        true
+    }
+
+    fn visit(&mut self, cell: &Cell, val: isize) {
+        self.cells[cell.x as usize][cell.y as usize] = val;
+    }
+
+    fn visited(&self, cell: &Cell) -> bool {
+        self.cells[cell.x as usize][cell.y as usize] != UNVISITED
+    }
+
+    fn dump(&self) {
+        println!("{}", &self);
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for row in &self.cells {
+            for col in row.iter() {
+                write!(f, "{:3}", col)?;
+            }
+
+            writeln!(f)?;
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+struct Cell {
+    x: isize,
+    y: isize,
+}
+
+impl fmt::Display for Cell {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Count{{x: {}, y: {}}}", self.x, self.y)
+    }
+}
+
+fn find_tour(board: &mut Board, cell: Cell, count: isize) -> bool {
+    loop {
+        board.visit(&cell, count);
+        println!("visited: {} on move {}", &cell, count);
+
+        if let Some(next_cell) = board.next_move(cell) {
+            println!("next_cell: {next_cell}");
+            return find_tour(board, next_cell, count + 1);
+        } else {
+            break;
+        }
+    }
+
+    true
+}
+
+fn main() -> Result<()> {
+    let mut board = Board::new();
+    let start_cell = Cell { x: 0, y: 0 };
+    let start = Instant::now();
+    let success = find_tour(&mut board, start_cell, 0);
+    let duration = start.elapsed();
+
+    match success {
+        true => {
+            println!("Success! in {:?}", duration);
+            // board.dump();
+        }
+        false => println!("Could not find a tour in {:?}...", duration),
+    }
+    board.dump();
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod unit {
+    use super::*;
+
+    #[test]
+    fn test_board_new() {
+        let tut = Board::new();
+        for row in &tut.cells {
+            for col in row.iter() {
+                assert_eq!(*col, UNVISITED);
+            }
+        }
+    }
+
+    #[test]
+    fn test_board_visit() {
+        let mut tut = Board::new();
+        assert_eq!(tut.visited(&Cell { x: 0, y: 0 }), false);
+        tut.visit(&Cell { x: 0, y: 0 }, 1);
+        assert_eq!(tut.visited(&Cell { x: 0, y: 0 }), true);
+    }
+
+    #[test]
+    fn test_moves_from_corner() {
+        let tut = Board::new();
+        let moves = tut.moves(Cell { x: 0, y: 0 });
+        assert_eq!(moves.len(), 2);
+
+        let tut = Board::new();
+        let moves = tut.moves(Cell { x: 7, y: 7 });
+        assert_eq!(moves.len(), 2);
+
+        let tut = Board::new();
+        let moves = tut.moves(Cell { x: 7, y: 0 });
+        assert_eq!(moves.len(), 2);
+
+        let tut = Board::new();
+        let moves = tut.moves(Cell { x: 0, y: 7 });
+        assert_eq!(moves.len(), 2);
+
+        let mut tut = Board::new();
+        tut.visit(&Cell { x: 2, y: 1 }, 1);
+        let moves = tut.moves(Cell { x: 0, y: 0 });
+        assert_eq!(moves.len(), 1);
+
+        let mut tut = Board::new();
+        tut.visit(&Cell { x: 5, y: 6 }, 1);
+        let moves = tut.moves(Cell { x: 7, y: 7 });
+        assert_eq!(moves.len(), 1);
+
+        let mut tut = Board::new();
+        tut.visit(&Cell { x: 5, y: 6 }, 1);
+        tut.visit(&Cell { x: 6, y: 5 }, 2);
+        let moves = tut.moves(Cell { x: 7, y: 7 });
+        assert!(moves.is_empty());
+    }
+
+    #[test]
+    fn test_moves_from_middle() {
+        let tut = Board::new();
+        let moves = tut.moves(Cell { x: 4, y: 4 });
+        assert_eq!(moves.len(), 8);
+    }
+
+    #[test]
+    fn test_valid_move_from_corner() {
+        let mut tut = Board::new();
+        let start_cell = &Cell { x: 0, y: 0 };
+        tut.visit(start_cell, 0);
+        assert_eq!(tut.valid_move(start_cell, &(-1, -2)), false);
+        assert_eq!(tut.valid_move(start_cell, &(-1, 2)), false);
+        assert_eq!(tut.valid_move(start_cell, &(-2, 1)), false);
+        assert_eq!(tut.valid_move(start_cell, &(1, -2)), false);
+        assert_eq!(tut.valid_move(start_cell, &(1, 2)), true);
+        assert_eq!(tut.valid_move(start_cell, &(2, -1)), false);
+        assert_eq!(tut.valid_move(start_cell, &(2, -1)), false);
+        assert_eq!(tut.valid_move(start_cell, &(2, 1)), true);
+    }
+
+    #[test]
+    fn test_valid_move_from_middle() {
+        let mut tut = Board::new();
+        let start_cell = &Cell { x: 3, y: 3 };
+        tut.visit(start_cell, 0);
+        assert_eq!(tut.valid_move(start_cell, &(-1, -2)), true);
+        assert_eq!(tut.valid_move(start_cell, &(-1, 2)), true);
+        assert_eq!(tut.valid_move(start_cell, &(-2, 1)), true);
+        assert_eq!(tut.valid_move(start_cell, &(1, -2)), true);
+        assert_eq!(tut.valid_move(start_cell, &(1, 2)), true);
+        assert_eq!(tut.valid_move(start_cell, &(2, -1)), true);
+        assert_eq!(tut.valid_move(start_cell, &(2, -1)), true);
+        assert_eq!(tut.valid_move(start_cell, &(2, 1)), true);
+    }
+}
